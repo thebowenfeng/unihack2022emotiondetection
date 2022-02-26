94719635afd3faf925214483717a7c910a050294
Fengax <857514.leofeng@gmail.com>
2022-02-26 22:59:06 +1100

    added CORS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
 
 
