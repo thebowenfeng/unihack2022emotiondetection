c124b23ba8cae5d415446cfdb098acbac4da5b99
Fengax <857514.leofeng@gmail.com>
2022-02-27 00:22:23 +1100

    added chatbot

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import transformers
 from flask import Flask, request, jsonify
 from transformers import pipeline
 from flask_cors import CORS
@@ -5,6 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
+chatbot = pipeline("conversational", model="microsoft/DialoGPT-medium")
 
 
 @app.route('/')
@@ -18,6 +20,16 @@ def detect_emotion():
         return "No text provided"
     else:
         return jsonify(emotion(request.args.get("text")))
+
+
+@app.route("/chat")
+def chat():
+    if request.args.get("text") is None:
+        return "No text provided"
+    else:
+        result_str = str(chatbot(transformers.Conversation(request.args.get("text")), pad_token_id=50256))
+        bot_resp = result_str.find("bot >>")
+        return result_str[bot_resp + 6:]
 
 
 if __name__ == '__main__':
